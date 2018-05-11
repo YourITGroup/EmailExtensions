fb16c922fac14c3b385435e640aac1a439e6c6ed
Robert Foster <robert@refoster.com.au>
2018-05-11 13:22:18 +1000

    upgraded nuget executable

--- Refactored.Email/StringExtensions.cs
+++ Refactored.Email/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Refactored.Email
 {
@@ -32,6 +33,60 @@ namespace Refactored.Email
             result.Append(s, pos, s.Length - pos);
 
             return result.ToString();
+        }
+
+        /// <summary>Expands the url with a standard full url based on the baseUrl.</summary>
+        /// <param name="url"></param>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        internal static string ExpandUrl(this string url, string baseUrl)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                return url;
+            }
+
+            if (url.StartsWith("~/"))
+            {
+                return url.Replace("~/", $"{baseUrl}/");
+            }
+            else if (url.StartsWith("/http:") || url.StartsWith("/https:"))
+            {
+                return url.Substring(1);
+            }
+            else if (url.StartsWith("/"))
+            {
+                return $"{baseUrl}{url}";
+            }
+            else
+            {
+                return $"{baseUrl}/{url}";
+            }
+
+        }
+
+        /// <summary>
+        /// Expands out local urls (those not containing any server/scheme parts) to full urls.
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        internal static string ExpandUrls(this string content, string baseUrl)
+        {
+            foreach (Match match in new Regex("(?<fullHref>(href=(?<quote>\"|'))(?<url>[^\"|']+)(\\k<quote>))").Matches(content))
+            {
+                string url = match.Groups["url"].Value;
+                string fullHref = match.Groups["fullHref"].Value;
+
+                string newValue = url.ExpandUrl(baseUrl);
+                string replacement = fullHref.Replace(url, newValue);
+
+                if (content.Contains(fullHref) && !(url == newValue))
+                {
+                    content = Regex.Replace(content, Regex.Escape(fullHref), replacement, RegexOptions.IgnoreCase);
+                }
+            }
+            return content;
         }
 
     }
--- Refactored.UmbracoEmailExtensions/Properties/AssemblyInfo.cs
+++ Refactored.UmbracoEmailExtensions/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Refactored.UmbracoEmailExtensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Digitalsmith Pty Ltd")]
+[assembly: AssemblyCompany("Quantum Sky")]
 [assembly: AssemblyProduct("Refactored.UmbracoEmailExtensions")]
-[assembly: AssemblyCopyright("Copyright © Digitalsmith 2011-2014")]
+[assembly: AssemblyCopyright("Copyright © Robert Foster 2011-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.1")]
-[assembly: AssemblyFileVersion("2.1.1.1")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
